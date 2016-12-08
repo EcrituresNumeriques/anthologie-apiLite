1fedb9519a1a993c91ea0a41769b46ca5b5c1822
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-12-08 17:12:03 -0500

    find typo

--- src/app/side/app.js
+++ src/app/side/app.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
       $entity = $("<article/>");
       $datalist = $("<option/>");
       $datalist.attr("value",data.entities[i].id).html(data.entities[i].title);
-      $("findEntity").append($datalist);
+      $("#findEntity").append($datalist);
       $entity.attr("id","entity"+data.entities[i].id).data("id",data.entities[i].id).addClass("entity");
       $entity.append('<p/>');
       $entity.children("p").html(data.entities[i].title);
