8f0fcf3fae9de42c68ab35b0d67d8a2ec9b8007f
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-12-08 14:08:08 -0500

    auteurs

--- src/get/authors/all.php
+++ src/get/authors/all.php
@@ -2,15 +2,22 @@
 
 //select info in database : Model
 try{
-$getAuthors = $db->prepare("SELECT * FROM authors");
+$getAuthors = $db->prepare("SELECT * FROM authors a JOIN author_translations at ON a.id = at.author_id");
 $getAuthors->execute();
 }
 catch(Exception $e){
   errorJSON('SQL error : ' . $e->getMessage(),500);
 }
 //treat information : Controler
-
+$intermediaire = $getAuthors->fetchAll(PDO::FETCH_ASSOC);
+$authors = array();
+foreach ($intermediaire as $author) {
+  $authors[$author['id']] = $author;
+}
 
 //put in $data : Vue
-$data['authors'] = $getAuthors->fetchAll(PDO::FETCH_ASSOC);
+$data['authors'] = $authors
+
+
+
 ?>
