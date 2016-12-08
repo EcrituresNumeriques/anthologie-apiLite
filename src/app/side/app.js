eec574b15b1725ccf36cb631f0fb7735f9cf50fa
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-12-08 18:44:52 -0500

    feed second column

--- src/app/side/app.js
+++ src/app/side/app.js
@@ -9,26 +9,37 @@ $(document).ready(function(){
 //functions
 
 //feed left column + actions
-  function displayEntities(data){
-    $("#entities > section").html('');
-    for(i=0;i<data.entities.length;i++){
-      $entity = $("<article/>");
-      $entity.attr("id","entity"+data.entities[i].id).data("id",data.entities[i].id).addClass("entity");
-      $entity.append('<p/>');
-      $entity.children("p").html(data.entities[i].title);
-      $("#entities > section").append($entity);
-    }
-    $("#findEntityInput").on("input",function(){
-      $("#entities > section > article").addClass("hidden");
-      $("#entities > section > article > p:contains("+$(this).val()+")").parent("article").removeClass("hidden");
-    });
-    $(".entity").on("click",loadEntity);
+function displayEntities(data){
+  $("#entities > section").html('');
+  for(i=0;i<data.entities.length;i++){
+    $entity = $("<article/>");
+    $entity.attr("id","entity"+data.entities[i].id).data("id",data.entities[i].id).addClass("entity");
+    $entity.append('<p/>');
+    $entity.children("p").html(data.entities[i].title);
+    $("#entities > section").append($entity);
   }
+  $("#findEntityInput").on("input",function(){
+    $("#entities > section > article").addClass("hidden");
+    $("#entities > section > article > p:contains("+$(this).val()+")").parent("article").removeClass("hidden");
+  });
+  $(".entity").on("click",loadEntity);
+}
 
 //load entity in the second row
-  function loadEntity(){
-    $(this).css("background-color","red");
-  }
+function loadEntity(){
+  $.get("/v1/entities/"+$(this).data("id")).done(displayEntity);
+}
 
+//display Entity in the second column
+function displayEntity(data){
+  $("#entity > section").html('');
+  for(i=0;i<data.entities.length;i++){
+    $entity = $("<article/>");
+    $entity.attr("id","entity"+data.entities[i].id).data("id",data.entities[i].id).addClass("entity");
+    $entity.append('<p/>');
+    $entity.children("p").html(data.entities[i].title);
+    $("#entities > section").append($entity);
+  }
+}
 
 });
