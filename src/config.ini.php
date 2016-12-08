72c96f92b7dbc1d42496cf36324eaf3196001c39
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-12-08 09:25:25 -0500

    database connect

--- src/config.ini.php
+++ src/config.ini.php
@@ -1,5 +1,16 @@
 <?php
 //connect database
+try
+{
+    $bdd = new PDO("mysql:host=".$dbCredential['host'].";dbname=".$dbCredential['base'], $dbCredential['user'], $dbCredential['pass']);
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+$unset($dbCredential);
+$bdd->query("SET NAMES UTF8");
+
 
 //include function
 include('function.php');
---
+++ src/config.key.ini.php
@@ -0,0 +1,8 @@
+<?php
+$dbCredential = array(
+  "user" => "YOUR_USER",
+  "base" => "YOUR_DATABASE",
+  "pass" => "YOUR_PASSWORD",
+  "host" => "localhost"
+);
+?>
