e4372cf0934956118dcf0cd241cc87a267eb621b
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-12-08 15:03:47 -0500

    for loop

--- src/app/side/app.js
+++ src/app/side/app.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   //get entities
   $.get("/v1/entities").done(function(data){
     $("#entities > section").html();
-    for each(entity in data.entities){
+    for(i=0;i<data.entities.length;i++){
       $entity = $("<article/>");
-      $entity.attr("id","entity"+entity.id);
+      $entity.attr("id","entity"+data.entities[i].id);
       $("#entities > section").append($entity);
     }
   });
