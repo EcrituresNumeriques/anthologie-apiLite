cff071cfd489e5808151fbb4faa26ccaca43476e
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-12-08 18:08:25 -0500

    switch label/value

--- src/app/side/app.js
+++ src/app/side/app.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
     for(i=0;i<data.entities.length;i++){
       $entity = $("<article/>");
       $datalist = $("<option/>");
-      $datalist.attr("value",data.entities[i].id).attr("label",data.entities[i].title);
+      $datalist.attr("value",data.entities[i].title).attr("label",data.entities[i].id);
       $("#findEntity").append($datalist);
       $entity.attr("id","entity"+data.entities[i].id).data("id",data.entities[i].id).addClass("entity");
       $entity.append('<p/>');
