7f854dc3a64581406bb83d9a1a29312bc3432519
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-12-09 03:11:20 -0500

    consolidate author in /entity/1

--- src/get/entities/one.php
+++ src/get/entities/one.php
@@ -5,7 +5,7 @@ try{
   $getEntities = $db->prepare("SELECT e.id as id_entity,e.book_id,e.era_id,e.genre_id,e.title,e.date,e.date_range FROM entities e WHERE id = :id");
   $getEntities->bindParam(":id",$_GET['entity']);
   $getEntities->execute();
-  
+
   $getAuthors = $db->prepare("SELECT a.*,at.name,l.name as lang,l.family FROM entities_authors_assoc eaa JOIN authors a ON eaa.authors_id = a.id JOIN author_translations at ON a.id = at.author_id JOIN languages l ON at.language_id = l.id WHERE eaa.entities_id = :id");
   $getAuthors->bindParam(":id",$_GET['entity']);
   $getAuthors->execute();
@@ -19,13 +19,31 @@ catch(Exception $e){
 }
 
 
-
-
 //treat information : Controler
+
+$intermediaire = $getAuthors->fetchAll(PDO::FETCH_ASSOC);
+$authors = array();
+foreach ($intermediaire as $author) {
+  if(empty($authors[$author['id']])){
+    $authors[$author['id']] = array(
+      "id" => $author['id'],
+      "born" => $author['born'],
+      "died" => $author['died'],
+      "born_range" => $author['born_range'],
+      "died_range" => $author['died_range'],
+    );
+  }
+  $authors[$author['id']]['name'][] = array(
+    "name"=>$author['name'],
+    "lang"=>$author['lang'],
+    "family"=>$author['family']
+  );
+}
+
 
 
 //put in $data : Vue
 $data['entities'] = $getEntities->fetchAll(PDO::FETCH_ASSOC);
-$data['entities'][0]['authors'] = $getAuthors->fetchAll(PDO::FETCH_ASSOC);
+$data['entities'][0]['authors'] = $authors;
 $data['entities'][0]['titleTranslation'] = $getTitleTranslation->fetchAll(PDO::FETCH_ASSOC);
 ?>
