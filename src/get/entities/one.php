3f35f24f2243bbf65fb27474cacdd080d38c0ee4
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-12-09 02:34:04 -0500

    one entity authors

--- src/get/entities/one.php
+++ src/get/entities/one.php
@@ -5,6 +5,9 @@ try{
   $getEntities = $db->prepare("SELECT e.id as id_entity,e.book_id,e.era_id,e.genre_id,e.title,e.date,e.date_range FROM entities e WHERE id = :id");
   $getEntities->bindParam(":id",$_GET['entity']);
   $getEntities->execute();
+  $getAuthors = $db->prepare("SELECT a.*,at.name,l.name as lang,l.family FROM entities_author_assoc eaa JOIN authors a ON eaa.authors_id = a.id JOIN author_translations at ON a.id = at.author_id JOIN languages l ON at.language_id = l.id WHERE eaa.entities_id = :id");
+  $getAuthors->bindParam(":id",$_GET['id']);
+  $getAuthors->execute();
 }
 catch(Exception $e){
   errorJSON('SQL error : ' . $e->getMessage(),500);
@@ -14,4 +17,5 @@ catch(Exception $e){
 
 //put in $data : Vue
 $data['entities'] = $getEntities->fetchAll(PDO::FETCH_ASSOC);
+$data['authors'] = $getAuthors->fetchAll(PDO::FETCH_ASSOC);
 ?>
