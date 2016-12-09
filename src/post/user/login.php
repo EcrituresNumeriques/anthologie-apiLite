615be3e709922a11307f2cdae385fa63155d5f88
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-12-09 06:12:10 -0500

    token + cleanup

--- src/post/user/login.php
+++ src/post/user/login.php
@@ -16,15 +16,17 @@ if($checkUser->rowCount() < 1){
 $user = $checkUser->fetch(PDO::FETCH_ASSOC);
 
 //Generate hashed password from user
-$password = $_POST['password'];
-$salt = $user['salt'];
-$salted = $password.'{'.$salt.'}';
-$digest = hash('sha512', $salted, true);
+$intermed['password'] = $_POST['password'];
+$intermed['salted'] = $intermed['password'].'{'.$user['salt'].'}';
+$intermed['digest'] = hash('sha512', $intermed['salted'], true);
 
 for ($i=1; $i<5000; $i++) {
-    $digest = hash('sha512', $digest.$salted, true);
+    $intermed['digest'] = hash('sha512', $intermed['digest'].$intermed['salted'], true);
 }
-$encodedPassword = base64_encode($digest);
+$encodedPassword = base64_encode($intermed['digest']);
+
+//cleanup intermeds
+unset($intermed);
 
 //Compare with password in database
 if($encodedPassword == $user['password']){
@@ -32,7 +34,10 @@ if($encodedPassword == $user['password']){
   $data['username'] = $user['username'];
   $data['success'] = 1;
 
-  //todo generate TOKEN HERE
+  //generate TOKEN here : tableless token v1
+  $data['token']['time'] = time();
+  $data['token']['user'] = $user['id'];
+  $data['token']['token'] = hash('sha512', $encodedPassword.hash('sha512', $user['salt'].$data['token']['time'], true), true);
 }
 else{
     errorJSON('Password mismatch',500);
