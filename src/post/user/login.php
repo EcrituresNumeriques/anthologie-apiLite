882c456b344a76797c05a05e9047a624614ba962
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-12-09 05:48:19 -0500

    api side alpha

--- src/post/user/login.php
+++ src/post/user/login.php
@@ -1,16 +1,41 @@
 <?php
 
-$password = 'Gromlech';
-$salt = 'tirjvnflc28cgccg40ws0os0cok0ogs';
+try{
+$checkUser = $db->prepare("SELECT id,username, salt, password FROM fos_users WHERE username = :username");
+$checkUser->bindParam(":username",$_POST['username']);
+$checkUser->execute()
+}
+catch(Exception $e){
+  errorJSON('SQL error : ' . $e->getMessage(),500);
+}
+
+if($checkUser->rowCount() < 1){
+  errorJSON('User not found',500);
+}
+
+$user = $checkUser->fetch(PDO::FETCH_ASSOC);
+
+//Generate hashed password from user
+$password = $_POST['password'];
+$salt = $user['salt'];
 $salted = $password.'{'.$salt.'}';
 $digest = hash('sha512', $salted, true);
 
 for ($i=1; $i<5000; $i++) {
     $digest = hash('sha512', $digest.$salted, true);
 }
-
 $encodedPassword = base64_encode($digest);
-$data['password'] = $encodedPassword;
-$data['username'] = $encodedPassword;
-$data['success'] = 1;
+
+//Compare with password in database
+if($encodedPassword == $user['password']){
+  //success
+  $data['username'] = $user['username'];
+  $data['success'] = 1;
+
+  //todo generate TOKEN HERE
+}
+else{
+    errorJSON('Password mismatch',500);
+}
+
 ?>
