0b01098dfafe41f2c32c16f88b518cd1da49192b
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-12-09 08:16:11 -0500

    newEntity test

--- src/post/entities/new.php
+++ src/post/entities/new.php
@@ -2,6 +2,16 @@
 if(empty($_POST['title'])){
   errorJSON("no title",400);
 }
-$data['newEntityId'] = 2;
+try{
+$insertNewEntity = $db->prepare("INSERT INTO entities (title, created_at, updated_at) VALUES (:title,NOW(),NOW())");
+$insertNewEntity->bindParam(":title",$_POST['title']);
+$insertNewEntity->execute();
+}
+catch(Exception $e){
+  errorJSON('SQL error : ' . $e->getMessage(),500);
+}
+
+
+$data['newEntityId'] = $db->lastInsertId();
 
 ?>
