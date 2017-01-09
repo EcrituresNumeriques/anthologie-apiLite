68abe39d9b78cfa4c24ceae84e27e9261a00d8b1
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-01-09 04:00:23 -0500

    typo mysql table

--- src/post/translations/new.php
+++ src/post/translations/new.php
@@ -1,6 +1,6 @@
 <?php
 try{
-$insertNewTranslation = $db->prepare("INSERT INTO entities_translation (entity_id, user_id, group_id, language_id, text_translated, created_at, updated_at) VALUES (:entity,:user,:group,:language,:text,NOW(),NOW())");
+$insertNewTranslation = $db->prepare("INSERT INTO entities_translations (entity_id, user_id, group_id, language_id, text_translated, created_at, updated_at) VALUES (:entity,:user,:group,:language,:text,NOW(),NOW())");
 $insertNewTranslation->bindParam(":entity",$_POST['entity']);
 $insertNewTranslation->bindParam(":user",$user['user']['id']);
 (!empty($user['user']['groups'][0])?:$user['user']['groups'][0] = NULL);
