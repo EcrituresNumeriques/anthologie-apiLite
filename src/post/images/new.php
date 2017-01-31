a43c6da7b75cf8601f649fcae541cb500dc82cdd
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-01-31 16:12:05 -0500

    get image

--- src/post/images/new.php
+++ src/post/images/new.php
@@ -1,5 +1,5 @@
 <?php
-if(is_numeric($_POST['entity']) && (!empty($_POST['url']) OR !empty($_POST['file']))){
+if(is_numeric($_POST['entity']) && (!empty($_POST['url']) OR !empty($_FILES['file']))){
   try{
   $insertNewImage = $db->prepare("INSERT INTO images (user_id,group_id,URL) VALUES (:user.:group,:url)");
   $insertNewImage->bindParam(":url",$_POST['url']);
