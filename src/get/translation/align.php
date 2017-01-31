cade8a8795dd357c82f04de9f4f8f129470a1c58
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-01-31 17:32:39 -0500

    typos

--- src/get/translation/align.php
+++ src/get/translation/align.php
@@ -2,7 +2,7 @@
 
 $getTranslations = $db->prepare("SELECT * FROM `entities_translations` where id = :id OR id = :id2");
 $getTranslations->bindParam(":id",$_GET['id']);
-$getTranslations->bindParam(":id",$_GET['id2']);
+$getTranslations->bindParam(":id2",$_GET['id2']);
 $getTranslations->execute() or die('Unable to get translations');
 $data['translation'] = $getTranslations->fetchAll(PDO::FETCH_ASSOC);
 
