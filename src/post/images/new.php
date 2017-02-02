aed9da5f83ed6adf0906a968a7135a28d7a765d7
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-02-01 21:14:38 -0500

    add url

--- src/post/images/new.php
+++ src/post/images/new.php
@@ -1,47 +1,14 @@
 <?php
-if(is_numeric($_POST['entity']) && (!empty($_POST['url']) OR !empty($_FILES['file']))){
-
-
-  //If file is provided, this is the source
-  if(!empty($_FILES['file'])){
-    $raw = $_FILES["file"]["tmp_name"];
-  }
-  else{
-    $raw = $_POST['url'];
-  }
-
-  if (!empty($raw)) {
-      // AJAX call
-      $time = time();
-      $path = "media/img/";
-      if (!file_exists($path)) {
-          //echo("creating folder");
-          mkdir($path, 0777, true);
-      }
-      $myUpload = file_get_contents($raw);
-      $image = imagecreatefromstring($myUpload);
-      if($image !== false){
-          $imageName = $user['user']['id']."-".time()."cover.jpg";
-          imagealphablending($image, FALSE);
-          imagesavealpha($image, TRUE);
-          imagejpeg($image,$path.$imageName,75);
-      }
-      else{
-        errorJSON("Unable to process image",400);
-      }
-
-
-      //file_put_contents('media/img/'.$_POST['campaign']."/".$fn,$myUpload);
-  }
-  else{
-      errorJSON("Unable to access image",400);
-  }
-
-
+if(is_numeric($_POST['entity']) && !empty($_POST['url'])){
+  //TODO: If a file is provided, grab a copy on the server
 
   try{
-  $insertNewImage = $db->prepare("INSERT INTO images (user_id,group_id,URL) VALUES (:user.:group,:url)");
+  $insertNewImage = $db->prepare("INSERT INTO images (user_id,group_id,URL,title,credit) VALUES (:user.:group,:url,:title,:credit)");
   $insertNewImage->bindParam(":url",$_POST['url']);
+  $title = (!empty($_POST['title'])?NULL:$_POST['title']);
+  $insertNewImage->bindParam(":title",$title);
+  $credit = (!empty($_POST['credit'])?NULL:$_POST['credit']);
+  $insertNewImage->bindParam(":credit",$credit);
   $insertNewImage->bindParam(":user",$user['user']['id']);
   (!empty($user['user']['groups'][0])?:$user['user']['groups'][0] = NULL);
   $insertNewImage->bindParam(":group",$user['user']['groups'][0]);
