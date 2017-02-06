eb212382e445716d915a2960e15fc281c5771f63
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-02-06 04:07:02 -0500

    actual word

--- src/lycee/side/lycee.js
+++ src/lycee/side/lycee.js
@@ -313,7 +313,8 @@ $(document).ready(function(){
         //for every word, append to $text
         for (var k = 0; k < data[i][j].length; k++) {
           if(typeof(data[i][j][k].t) !== "undefined" && data[i][j][k].t != "")  {
-            $word = $(' <span id="'+i+'-'+word+k+'">'+data[i][j][k].t+'</span> ');
+            currentWord = Number(word)+Number(k);
+            $word = $(' <span id="'+i+'-'+currentWord+'">'+data[i][j][k].t+'</span> ');
           }
           else{
             $word = " "+data[i][j][k].p+" ";
