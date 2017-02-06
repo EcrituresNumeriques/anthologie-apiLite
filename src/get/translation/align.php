4b71b1a3c6cb5ce3a5ba3558354d59b780d8442b
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-02-06 03:28:07 -0500

    functions

--- src/get/translation/align.php
+++ src/get/translation/align.php
@@ -30,7 +30,11 @@ else{
   $getTranslations->execute() or die('Unable to get translations');
   $translations = $getTranslations->fetchAll(PDO::FETCH_ASSOC);
   $json = array();
+  $i = 0;
   foreach ($translations as $translation) {
+    $json[] = stropheJSON($translation['text_translated'],$i);
+    $i++;
+    /*
     //generate lines by line
     $lines = explode("\n",$translation['text_translated']);
     $jsonLine = array();
@@ -44,9 +48,53 @@ else{
     }
     //add this line to json
     $json[] = $jsonLine;
+    */
   }
   $data['align']['json'] = $json;
+
+
+
+
 }
+
+
+function stropheJSON($strophe,$textNbr){
+    $w = 0;
+    $return = array();
+    $parts = explode('\n', $strophe);
+    foreach($parts as $value){
+        $tmp = versJSON($value,$w,$textNbr);
+        $w += count($tmp);
+        $return[] = $tmp;
+    }
+    return $return;
+}
+
+
+function versJSON($vers,$word,$textNbr){
+    $return = array();
+    $string = preg_match_all("/\p{Greek}+|\w+|\p{P}+/u",$vers,$matches);
+    foreach($matches[0] as $value){
+      $tmp = array();
+        $word++;
+        if(preg_match("/\w+/", $value)){
+          $tmp['t'] = $value;
+          $tmp['h'] = array_fill(0,$textNbr,array());
+          $tmp['h'][$textNbr] = $word;
+        }
+        elseif(preg_match("/\p{Greek}+/u", $value)){
+          $tmp['t'] = $value;
+          $tmp['h'] = array_fill(0,$textNbr,array());
+          $tmp['h'][$textNbr] = $word;
+        }
+        else{
+          $tmp['p'] = $value;
+        }
+        $return[] = $tmp;
+    }
+    return $return;
+}
+
 
 
 /*
