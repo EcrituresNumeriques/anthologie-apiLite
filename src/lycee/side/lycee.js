7509ce3533c42a8206be9ca79fccc84fb70b5fc4
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-02-06 23:45:14 -0500

    event on span

--- src/lycee/side/lycee.js
+++ src/lycee/side/lycee.js
@@ -314,7 +314,9 @@ $(document).ready(function(){
         for (var k = 0; k < data[i][j].length; k++) {
           if(typeof(data[i][j][k].t) !== "undefined" && data[i][j][k].t != "")  {
             currentWord = Number(word)+Number(k);
-            $word = $('<span id="'+i+'-'+currentWord+'" data-text="'+i+'" data-vers="'+j+'" data-word="'+k+'">'+data[i][j][k].t+'</span>').addClass("highlight");
+            $word = $('<span id="'+i+'-'+currentWord+'" data-text="'+i+'" data-vers="'+j+'" data-word="'+k+'">'+data[i][j][k].t+'</span>').addClass("highlight").on("click",function(){
+              console.log("hello click");
+            });
           }
           else{
             $word = " "+data[i][j][k].p+" ";
