2cf5fd77447b1ec06d04cf6720af5c8253960d50
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-02-07 00:49:52 -0500

    show actual text

--- src/lycee/side/lycee.js
+++ src/lycee/side/lycee.js
@@ -335,7 +335,7 @@ $(document).ready(function(){
 
   function clickHighlight(data,$el){
     console.log(data);
-    console.log($el.data("text"));
+    console.log(data[$el.data("text")][$el.data("vers")][$el.data("word")].t);
   }
 
   function sendAlign(){
