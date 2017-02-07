dbec2e6a10170e68da717fcaf4ae52fe11284085
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-02-07 04:15:33 -0500

    sqlquery

--- src/post/align/update.php
+++ src/post/align/update.php
@@ -11,11 +11,11 @@ if(count(json_decode($_POST['data'])) != 2){
   errorJSON('JSON data align not proper JSON',400);
 }
 
-$setAlign = $db->prepare("INSERT INTO entities_translations_align (pair,json,user_id,group_id,created_at,modified_at) VALUES (:pair,:json,:user,:group,NOW(), NOW()) ON DUPLICATE KEY UPDATE SET modified_at = NOW(), json = :json");
+$setAlign = $db->prepare("INSERT INTO entities_translations_align (pair,json,user_id,group_id,created_at,modified_at) VALUES (:pair,:json,:user,:group,NOW(), NOW()) ON DUPLICATE KEY UPDATE modified_at = NOW(), json = :json");
 $setAlign->bindParam(":pair",$_POST['pair']);
 $setAlign->bindParam(":json",$_POST['data']);
 $setAlign->bindParam(":user",$user['user']['id']);
 (!empty($user['user']['groups'][0])?:$user['user']['groups'][0] = NULL);
 $setAlign->bindParam(":group",$user['user']['groups'][0]);
-$setAlign->execute() or die('unable to get alignements');
+$setAlign->execute() or die('unable to set align');
 ?>
