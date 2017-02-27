b7a063559b115f57a295ee44db0e5c3374c15050
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-02-26 22:05:55 -0500

    assoc keywords

--- src/post/keywords/assoc.php
+++ src/post/keywords/assoc.php
@@ -17,6 +17,7 @@ $getKeyword->execute();
 if($getKeyword->rowCount() > 0){
   $keyword = $getKeyword->fetch(PDO::FETCH_ASSOC);
   $keyword = $keyword['keyword_id'];
+  $data['KeywordFound'] = $keyword;
 }
 else{
   if(empty($_POST['description'])){
@@ -34,12 +35,24 @@ $insertNewkeyword->bindParam(":user",$user['user']['id']);
 $insertNewkeyword->bindParam(":group",$user['user']['groups'][0]);
 $insertNewKeyword->execute();
 $keyword = $db->lastInsertId();
+$data['newKeyword'] = $keyword;
 //add new translation
-
+$insertTranslation = $db->prepare("INSERT INTO keywords_translations kt (keyword_id,user_id,group_id,language_id,title,description,created_at,updated_at) VALUES (:keyword,:user,:group,:lang,:text,:description,NOW(),NOW())");
+$insertTranslation->bindParam(":keyword",$keyword);
+$insertTranslation->bindParam(":user",$user['user']['id']);
+(!empty($user['user']['groups'][0])?:$user['user']['groups'][0] = NULL);
+$insertTranslation->bindParam(":group",$user['user']['groups'][0]);
+$insertTranslation->bindParam(":lang",$_POST['language']);
+$insertTranslation->bindParam(":text",$_POST['text']);
+$insertTranslation->bindParam(":description",$_POST['description']);
+$insertTranslation->execute();
 }
 
 //Assoc to entity
-
+$assocKeyword = $db->prepare("INSERT INTO entities_keywords_assoc eka (entities_id,keywords_id) VALUES (:entity,:keyword) ON DUPLICATE KEY UPDATE keyword_id = keywords_id");
+$assocKeyword->bindParam(":entity",$_POST['entity']);
+$assocKeyword->bindParam(":keyword",$keyword);
+$assocKeyword->execute();
 
 }
 catch(Exception $e){
